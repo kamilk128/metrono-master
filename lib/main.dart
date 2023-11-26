723884b82108ff0611524d451da028ed4dbfbc36
kamilk128 <kkoldunek@gmail.com>
2023-11-26 20:17:23 +0100

    navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'metronome.dart';
+import 'navigation.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
           appBar: AppBar(title: const Text("Metronome")),
-          body: const MetronomeControl()),
+          body: const NavigationPage()),
     );
   }
 }
---
+++ lib/navigation.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'metronome.dart';
+
+class NavigationPage extends StatefulWidget {
+  const NavigationPage({super.key});
+
+  @override
+  State<NavigationPage> createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const MetronomeControl();
+        break;
+      case 1:
+        page = const Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    void onItemTapped(int index) {
+      setState(() {
+        selectedIndex = index;
+      });
+    }
+
+    return Scaffold(
+      body: Center(
+        child: page,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: theme.colorScheme.primary,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Trening',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Lista treningów',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        unselectedItemColor: theme.colorScheme.onTertiary,
+        selectedItemColor: Colors.amber[800],
+        onTap: onItemTapped,
+      ),
+    );
+  }
+}
