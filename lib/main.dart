b04d144ad2ec24c9ad5dc953e0f980e113ee8d7d
Mikolaj Burdzinski <mikolaj.burdzinski@student.put.poznan.pl>
2024-01-16 23:17:48 +0100

    Post-installation tweaks
    - keep screen turned on while the app is running
    - change app name on apps list

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'models/rhythm.dart';
 import 'models/settings.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WakelockPlus.enable();
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       builder: (context, _) {
